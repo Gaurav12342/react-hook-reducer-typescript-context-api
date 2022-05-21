357a7039b547917f32f0dfa412fc4fdd123c7f24
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-05-21 18:58:55 +0530

    Added insert functionality

--- src/Crud/Form.tsx
+++ src/Crud/Form.tsx
@@ -1,22 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
+import { userContext } from './index';
+import { INSERT } from '../action/actionsTypes';
+
 
 const Form: FC = () => {
+    const data = useContext(userContext);
     return (
         <>
             <div>
                 <label>First Name : </label>
-                <input name="fname" type={'text'} />
+                <input name="fname" type={'text'} onChange={data.handleChange} />
             </div>
             <div>
                 <label>Last Name : </label>
-                <input name="lname" type={'text'} />
+                <input name="lname" type={'text'} onChange={data.handleChange} />
             </div>
             <div>
                 <label>City : </label>
-                <input name="city" type={'text'} />
+                <input name="city" type={'text'} onChange={data.handleChange} />
             </div>
             <div>
-                <button>Insert</button>
+                <button type='submit' onClick={() => data.dispatch({ type: INSERT })}>Insert</button>
             </div>
         </>
     )
--- src/Crud/index.tsx
+++ src/Crud/index.tsx
@@ -1,12 +1,62 @@
-import React, { FC } from 'react'
+import React, { createContext, FC, useReducer } from 'react'
 import Form from './Form';
-const index: FC = () => {
+import { INSERT, INITILIZE_VALUE, UPDATE, DELETE, GET } from '../action/actionsTypes';
+export interface IInitialState {
+    fname?: string;
+    lname?: string;
+    city?: string;
+    results?: any
+}
+
+
+export interface IAction {
+    type?: string;
+    payload?: any;
+}
+
+export interface IUserContext {
+    state?: IInitialState[],
+    dispatch?: any,
+    handleChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+export const userContext = createContext<IUserContext>({});
+
+const Crud: FC = () => {
+
+    const initialState: IInitialState = {
+        fname: "",
+        lname: "",
+        city: "",
+        results: []
+    };
+
+    const reducer = (state = initialState, action: IAction) => {
+        switch (action.type) {
+            case INITILIZE_VALUE:
+                return { ...state, ...action.payload }
+            case INSERT:
+                return { ...state, results: state.results.concat({ ...state, ...action.payload }) }
+            default:
+                return { ...state }
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, initialState);
+    console.log("State =>", state);
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        dispatch({ type: INITILIZE_VALUE, payload: { ...state, [name]: value } });
+
+    }
     return (
         <div>
             <h1>Crud</h1>
-            <Form />
+            <userContext.Provider value={{ state, dispatch, handleChange }}>
+                <Form />
+            </userContext.Provider>
         </div>
     )
 }
 
-export default index
+export default Crud
